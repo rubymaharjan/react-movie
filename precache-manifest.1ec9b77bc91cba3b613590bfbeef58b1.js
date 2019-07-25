a89480a4acfd81be02660066b163c24caea75eef
rubymaharjan <ruby3mhrzn@gmail.com>
2019-07-25 10:31:40 +0545

    Updates

---
+++ precache-manifest.1ec9b77bc91cba3b613590bfbeef58b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defb31ce4b94d7661a5deff80aeb6f00",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "b9ce784c97490d41dce2",
+    "url": "/react-movie/static/css/main.b2f677fe.chunk.css"
+  },
+  {
+    "revision": "1945b93926952e3cd4ab",
+    "url": "/react-movie/static/js/2.43ee8039.chunk.js"
+  },
+  {
+    "revision": "b9ce784c97490d41dce2",
+    "url": "/react-movie/static/js/main.2c499f7f.chunk.js"
+  },
+  {
+    "revision": "5de7cd1febd31cdee4f7",
+    "url": "/react-movie/static/js/runtime~main.b4244ab5.js"
+  }
+]);
