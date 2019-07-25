680a03235a2099e58dcb005729c150fdfe968cfc
rubymaharjan <ruby3mhrzn@gmail.com>
2019-07-25 10:17:24 +0545

    feat: production build

---
+++ build/precache-manifest.9f0f0b6d08a98d2c833bf8f788d1bc40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de60cb25d32d2a4467c0ec2f42b49da0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7687675a6e0c8be6b33",
+    "url": "/static/css/main.b2f677fe.chunk.css"
+  },
+  {
+    "revision": "1945b93926952e3cd4ab",
+    "url": "/static/js/2.43ee8039.chunk.js"
+  },
+  {
+    "revision": "d7687675a6e0c8be6b33",
+    "url": "/static/js/main.b69aa004.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
